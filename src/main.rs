4a8c6afaac89a9ddf94f649094067a10ca79bd52
mdrokz <33598169+mdrokz@users.noreply.github.com>
2021-12-25 00:19:22 +0530

    initial setup

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,18 @@
+use headless_chrome::{Browser, LaunchOptions};
+
 fn main() {
+    let browser = Browser::new(
+        LaunchOptions::default_builder()
+            .headless(false)
+            .build()
+            .unwrap(),
+    )
+    .unwrap();
+
+
+    let tab = browser.wait_for_initial_tab().unwrap();
+
+    
+
     println!("Hello, world!");
 }
